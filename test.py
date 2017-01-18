f410dd6871408f70a9703f1b86570b7eb96c2645
Chialin Chou <chialin668@gmail.com>
2017-01-17 21:21:42 -0800

    Test event

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from rx import Observable
 
 def handler(event, context):
+    #
+    print "Last: %s" % event['last_name']
+    print "First: %s" % event['first_name']
+
+    #
     xs = Observable.from_([1, 2, 3, 4, 5, 6])
     ys = xs.to_blocking()
     zs = (x*x for x in ys if x > 3)
