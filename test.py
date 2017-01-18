05ec727e8d36245c5312797020e5c7ee6faa38e0
Chialin Chou <chialin668@gmail.com>
2017-01-17 21:33:18 -0800

    remove import

--- test.py
+++ test.py
@@ -1,14 +1,4 @@
-from rx import Observable
-
-def handler(event, context):
-    #
+def lambda_handler(event, context):
     print "Last: %s" % event['last_name']
     print "First: %s" % event['first_name']
-
-    #
-    xs = Observable.from_([1, 2, 3, 4, 5, 6])
-    ys = xs.to_blocking()
-    zs = (x*x for x in ys if x > 3)
-    for x in zs:
-        print x
     return 'foobar'
